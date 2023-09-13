ec6413954f0873353d6f4f798a8f2c886369a311
Prakhar301101 <prakhar.pandey.pp6@gmail.com>
2023-09-13 11:24:11 +0530

    Change route import statement

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const Routes = require('../Server/routes/Routes');
+const Routes = require('./routes/Routes');
 const app = express();
 
 app.use(
