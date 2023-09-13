2ee14a9b24e94bac8ffd972d80e1bdb376afab65
Prakhar301101 <prakhar.pandey.pp6@gmail.com>
2023-09-13 11:09:44 +0530

    Change Cors origin

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose=require('mongoose')
-const cors=require('cors');
-const cookieParser = require('cookie-parser')
-const Routes=require('../Server/routes/Routes')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const Routes = require('../Server/routes/Routes');
 const app = express();
 
-
-app.use(cors({
-   credentials: true, 
-   origin:"http://127.0.0.1:5173"
-  }));
+app.use(
+  cors({
+    credentials: true,
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-mongoose.connect(process.env.MONGO_URL)
-
+mongoose.connect(process.env.MONGO_URL);
 
 app.use(Routes);
 
 let port = process.env.PORT;
-  app.listen(port, function () {
-    console.log(`Server started on ${port}`);
-  });
+app.listen(port, function () {
+  console.log(`Server started on ${port}`);
+});
